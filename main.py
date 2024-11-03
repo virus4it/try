352cbb57db741566a1c34830a295ca2b8aecb82c
virus4it <143338599+virus4it@users.noreply.github.com>
2024-11-03 15:46:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for item in numbers:
+        try:
+            result += item
+        except TypeError:
+            print(f"Некорректный тип данных для подсчёта суммы - {item}")
+            incorrect_data += 1
+    return result, incorrect_data
+
+def calculate_average(numbers):
+
+    if not isinstance(numbers, (list, tuple, set)):
+        print("В numbers записан некорректный тип данных")
+        return None
+
+
+    result, incorrect_data = personal_sum(numbers)
+    try:
+
+        count = len(numbers) - incorrect_data
+        return result / count
+    except ZeroDivisionError:
+        return 0
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
